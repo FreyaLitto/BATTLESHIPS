a20048feb0ac144d3bea53c4697db34d64d84ccf
FreyaLitto <>
2023-07-16 00:36:51 +0000

    Creating a grid for a battleship game

--- run.py
+++ run.py
@@ -20,4 +20,19 @@ import time
     3. "X" = part of ship that was hit with bullet
     4. "#" = water that was shot with bullet, a miss because it hit no ship
 """
-
+# Global variable for grid
+grid = [[]]
+# Global variable for grid size
+grid_size = 10
+# Global variable for number of ships to place
+num_of_ships = 2
+# Global variable for bullets left
+bullets_left = 50
+# Global variable for game over
+game_over = False
+# Global variable for number of ships sunk
+num_of_ships_sunk = 0
+# Global variable for ship positions
+ship_positions = [[]]
+# Global variable for alphabet
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
